8e4e86c8bf98d724f54607f2b3a95d2d980e442a
liuzixi <alexanderseabra123@gmail.com>
2026-03-09 10:39:25 +0800

    CSS的样式重置

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 
 import App from '@/App';
 
+import 'normalize.css';
 import '@/assets/css/index.less';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
