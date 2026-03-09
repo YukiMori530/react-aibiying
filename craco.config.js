eb4530ea2dc3cad35ec1b1e41b8b8ed0d64c1607
liuzixi <alexanderseabra123@gmail.com>
2026-03-09 10:13:45 +0800

    配置webpack别名，配置less别名，测试组件hello-world成功运行

---
+++ craco.config.js
@@ -0,0 +1,21 @@
+const CracoLessPlugin = require('craco-less');
+
+const path = require('path');
+
+const resolve = pathname => path.resolve(__dirname, pathname);
+
+module.exports = {
+  // 配置webpack别名
+  webpack: {
+    alias: {
+      '@': resolve('src'),
+      'utils': resolve('src/utils'),
+      'components': resolve('src/components'),
+    },
+  },
+  // 配置less
+  plugins: [
+    {
+      plugin: CracoLessPlugin
+    },]
+};
