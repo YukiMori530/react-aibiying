c2667c86d7a0bf1851566a72f18cf8a6d288f66a
liuzixi <alexanderseabra123@gmail.com>
2026-03-10 20:12:26 +0800

    box-shadow的动画效果和样式混入

--- src/assets/theme/index.js
+++ src/assets/theme/index.js
@@ -10,6 +10,14 @@ const theme = {
         secondaryColor: "#222"
     },
 
+    mixin:{
+        boxShadow:`
+            transition: box-shadow 0.2s ease;
+            &:hover{
+                box-shadow: 0 2px 4px rgba(0,0,0,0.18)
+            }
+        `
+    }
     
 }
 
