9a86fa0d6a8ef1a2250eff366be3eb92ffd13544
liuzixi <alexanderseabra123@gmail.com>
2026-03-26 15:00:50 +0800

    房间列表数据的展示

--- src/views/entire/c-cpns/entire-rooms/index.jsx
+++ src/views/entire/c-cpns/entire-rooms/index.jsx
@@ -1,10 +1,26 @@
 import React from "react";
 import { RoomsWrapper } from "./style";
+import { useSelector } from "react-redux";
+import { shallowEqual } from "react-redux";
+import RoomItem from "@/components/room-item";
 
 const EntireRooms = () => {
+    /*从redux中获取数据 */
+    const { roomList, totalCount } = useSelector((state) => ({
+        roomList: state.entire.roomList,
+        totalCount: state.entire.totalCount
+    }), shallowEqual)
+
     return (
         <RoomsWrapper>
-            rooms
+            <h2 className="title">共{totalCount}处住所</h2>
+            <div className="list">
+                {
+                    roomList.map((item) => {
+                        return <RoomItem itemData={item} itemWidth="20%" key={item.id}/>
+                    })
+                }
+            </div>
         </RoomsWrapper>
     )
 }
