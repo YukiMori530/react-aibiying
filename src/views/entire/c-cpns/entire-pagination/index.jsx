c502aa176626ab2beeb23de2acde7d7ff7d98374
liuzixi <alexanderseabra123@gmail.com>
2026-03-27 22:35:19 +0800

    页码的组件展示和展示数据算法

--- src/views/entire/c-cpns/entire-pagination/index.jsx
+++ src/views/entire/c-cpns/entire-pagination/index.jsx
@@ -1,10 +1,32 @@
 import React from "react";
 import { PaginationWrapper } from "./style";
+import Pagination from '@mui/material/Pagination';
+import { useSelector } from "react-redux";
 
 const EntirePagination = () => {
+
+    const {currentPage, totalCount, roomList} =useSelector((state)=>({
+        currentPage: state.entire.currentPage,
+        totalCount: state.entire.totalCount,
+        roomList: state.entire.roomList,
+    }))
+
+    const totalPage = Math.ceil(totalCount / 20)
+    const startCount = currentPage * 20 + 1
+    const endCount = (currentPage + 1) * 20
+
     return (
         <PaginationWrapper>
-            pagination
+            {
+                !!roomList.length && (
+                    <div className="info">
+                    <Pagination count={totalPage}/>
+                     <div className="desc">
+                         第 {startCount} - {endCount} 个房源, 共超过 {totalCount} 个
+                    </div>
+                    </div>
+                )
+            }
         </PaginationWrapper>
     )
 }
--- src/views/entire/c-cpns/entire-pagination/style.js
+++ src/views/entire/c-cpns/entire-pagination/style.js
@@ -1,5 +1,23 @@
 import styled from "styled-components";
 
 export const PaginationWrapper = styled.div`
+    display: flex;
+    justify-content: center;
+    
+    .info{
+        display: flex;
+        flex-direction: column;
+        align-items: center;
 
+        .MuiPaginationItem-page.Mui-selected{
+            background-color: #222;
+            color: #fff;
+        }
+
+        .desc{
+            margin-top: 16px;
+            color: #222;
+        }
+
+    }
 `
