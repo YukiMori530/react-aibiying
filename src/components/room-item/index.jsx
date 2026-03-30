b7cbad850e75a1b88afd12a9b329e96f0076ec65
liuzixi <alexanderseabra123@gmail.com>
2026-03-30 12:52:06 +0800

    RoomItem的轮播图实现效果

--- src/components/room-item/index.jsx
+++ src/components/room-item/index.jsx
@@ -1,11 +1,20 @@
-import React from "react";
+import React, { useRef } from "react";
 import PropTypes from "prop-types";
 import { ItemWrapper } from "./style";
+import { Carousel } from "antd";
 import { Rating } from "@mui/material";
+import IconArrowLeft from "@/assets/svg/icon-arrow-left";
+import IconArrowRight from "@/assets/svg/icon-arrow-right";
 
 const RoomItem = (props)=>{
 
     const { itemData ,itemWidth = "25%" } = props
+
+    const sliderRef=useRef()
+
+    function controlClickHandle(isRight=true){
+        isRight ? sliderRef.current.next(): sliderRef.current.prev()
+    }
 
 
     return (
@@ -13,8 +22,29 @@ const RoomItem = (props)=>{
         $itemWidth={itemWidth}
         >
             <div className="inner">
-                <div className="cover">
+                {/* <div className="cover">
                     <img src={itemData.picture_url} alt="" />
+                </div> */}
+                <div className="slider">
+                    <div className="control">
+                        <div className="btn left" onClick={e=>controlClickHandle(false)}>
+                            <IconArrowLeft width="30" height="30"/>
+                        </div>
+                        <div className="btn right" onClick={e=>controlClickHandle(true)}>
+                            <IconArrowRight width="30" height="30"/>
+                        </div>
+                    </div>
+                    <Carousel dots={false} ref={sliderRef}>
+                        {
+                            itemData?.picture_urls?.map(item => {
+                                return (
+                                    <div className="cover" key={item}>
+                                        <img src={item} alt="" />
+                                    </div>
+                                )
+                            })
+                        }
+                    </Carousel>
                 </div>
                 <div className="desc">
                     { itemData.verify_info.messages.join("·") }
