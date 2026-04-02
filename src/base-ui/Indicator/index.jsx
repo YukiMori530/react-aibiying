a341a9b353130c49f4847d6819893548f2271c2a
liuzixi <alexanderseabra123@gmail.com>
2026-04-02 13:04:39 +0800

    Indicator组件封装-左右特殊情况

--- src/base-ui/Indicator/index.jsx
+++ src/base-ui/Indicator/index.jsx
@@ -6,11 +6,22 @@ const Indicator = (props)=>{
 
     const { selectIndex } = props
     const contentRef = useRef()
+
     useEffect(()=>{
         const selectItemEl = contentRef.current.children[selectIndex]
-        const itemElOffsetLeft = selectItemEl.offsetLeft
+        const itemLeft = selectItemEl.offsetLeft
+        const itemWidth=selectItemEl.clientWidth
+
+        const contentWidth=contentRef.current.clientWidth
+        const contentScroll=contentRef.current.scrollWidth
         //获取selectIndex要滚动的距离
-        const distance = ``
+        let distance = itemLeft+itemWidth*0.5-contentWidth*0.5
+
+        if(distance < 0) distance=0//左边特殊情况
+        const totalDistance=contentScroll-contentWidth
+        if(distance>totalDistance)distance=totalDistance//右边特殊情况
+
+        contentRef.current.style.transform=`translate(${-distance}px)`
 
     },[selectIndex])
 
